c4e269eb3d4cef0e106f879f27a8f9b6ce19156c
Maiza Oliveira <maiza.conceicao@gmail.com>
2022-05-23 16:42:18 -0300

    adicionado aula 6

---
+++ AULA 6.py
@@ -0,0 +1,20 @@
+n1=int(input( ' digite um numero :'))
+n2=int(input( ' digite mais um numero :'))
+s= n1 + n2
+print( ' a soma entre {} e {} vale {}'.format(n1,n2,s))
+
+
+print(type(n1))
+#int = ex 7, -4, 0, 9875
+#float= ex 4.5, 0.076, -15.233, 7.0
+#boll=True ou False
+#str = 'ola', '7.5', ''
+
+
+n=input('digite algo: ')
+print(n.isnumeric())
+print(n.isalpha())
+
+
+
+
--- ex002 aula5.py
+++ ex002 aula5.py
@@ -3,3 +3,4 @@ print('é um prazer te conhecer {}!'.format(nome))
 
 nome= input(' digite seu nome: ')
 print('é um prazer te conhecer {}!'.format(nome))
+
