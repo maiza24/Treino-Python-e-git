16289c30d82f58b1c58147634f02cac7bf8ad703
Maiza Oliveira <maiza.conceicao@gmail.com>
2022-05-25 20:31:10 -0300

    adicionado ex003 e 004

---
+++ ex003 aula6.py
@@ -0,0 +1,7 @@
+n1=float(input( " Digite um valor: "))
+n2=float(input( " digite outro valor:"))
+s= n1 + n2
+print(' a soma entre {} e {} é igual a {} '.format(n1, n2, s))
+
+
+print(type(n1))
---
+++ ex004 aula6.py
@@ -0,0 +1,10 @@
+a=input(' Digite algo: ')
+print('o tipo primitvo desse valor é  ' , type(a))
+print(' so tem espaços?' , a.isspace())
+print('é um numero: ' , a.isnumeric())
+print('é alfanumerico ?{} '.format(a.isalnum()))
+
+
+
+
+
