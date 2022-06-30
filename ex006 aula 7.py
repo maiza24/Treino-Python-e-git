8f1e5c8a11122288a4f8a5e6ea6787f7af0b855d
Maiza Oliveira <maiza.conceicao@gmail.com>
2022-06-30 18:10:23 -0300

    adicionado ex 08.09.010 aula 7

--- ex006 aula 7.py
+++ ex006 aula 7.py
@@ -2,4 +2,6 @@ n1=int(input(' digite um numero: '))
 d= n1*2
 t=n1*3
 r=n1 **(1/2)
-print('o dobro é {}, o triplo é {} e a raiz quadrada é {}'.format(d, t, r))
+print('o dobro é {}, o triplo é {} e a raiz quadrada é {:.2f}'.format(d, t, r))
+
+
---
+++ ex008 aula7.py
@@ -0,0 +1,10 @@
+n=float(input(' digite uma medida em metros: '))
+cm= n / 0.010000
+mm= n / 0.0010000
+print(' o valor em centimetros é {} e em milimetros é {}'.format(cm,mm))
+
+
+n=float(input(' digite uma medida em metros: '))
+cm= n*100
+mm= n*1000
+print(' o valor em centimetros é {} e em milimetros é {}'.format(cm,mm))
